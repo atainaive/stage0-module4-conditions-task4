61c9232d448804d7a781a2d338baf88fb027b5c9
atainaive <atai.omorov@stud.vgtu.lt>
2022-06-28 13:55:10 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String result = "";
+        result = (first > second) ? "first" : "second";
     }
 }
